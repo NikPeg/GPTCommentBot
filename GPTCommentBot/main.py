1ecc153b22b102dace5a836f0bc6e6656cd5c61f
NikPeg <peganov.nik@gmail.com>
2023-08-12 22:21:30 +0300

    wall post

--- GPTCommentBot/main.py
+++ GPTCommentBot/main.py
@@ -3,5 +3,5 @@ import config
 import constants
 
 
-api = vk.API(access_token=config.ACCESS_TOKEN)
-print(api.users.get(user_ids=1, v=constants.VK_VERSION))
+api = vk.API(access_token=config.ACCESS_TOKEN, v=constants.VK_VERSION)
+print(api.wall.post(message="Жаль, что не все поймут) Не многие вспомнят))"))
