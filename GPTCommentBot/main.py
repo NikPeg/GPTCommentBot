49a54573c626de26c3b7ee15060bfae729363aec
NikPeg <peganov.nik@gmail.com>
2023-08-12 22:46:42 +0300

    Make a method

--- GPTCommentBot/main.py
+++ GPTCommentBot/main.py
@@ -3,8 +3,12 @@ import config
 import constants
 
 
-api = vk.API(access_token=config.ACCESS_TOKEN, v=constants.VK_VERSION)
-# print(api.wall.post(message="Жаль, что не все поймут) Не многие вспомнят))"))
-post_id = 21
-user_id = 521780797
-print(api.wall.createComment(owner_id=user_id, post_id=post_id, message="Да уж, не так уж много олдов в наше время)"))
+def create_comment(api: vk.API, owner_id: int, post_id: int, message: str) -> int:
+    """Creates a comment in specified wall and post, returns its id"""
+    res: dict = api.wall.createComment(owner_id=owner_id, post_id=post_id, message=message)
+    return res.get("comment_id")
+
+
+if __name__ == "__main__":
+    api = vk.API(access_token=config.ACCESS_TOKEN, v=constants.VK_VERSION)
+    print(create_comment(api, -205429509, 12, "Бывают же люди в наше время"))
