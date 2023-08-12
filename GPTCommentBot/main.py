e1c768cf1a2108b4ae12dfb9e99910f8e3e32013
NikPeg <peganov.nik@gmail.com>
2023-08-12 22:30:10 +0300

    Post comments

--- GPTCommentBot/main.py
+++ GPTCommentBot/main.py
@@ -4,4 +4,7 @@ import constants
 
 
 api = vk.API(access_token=config.ACCESS_TOKEN, v=constants.VK_VERSION)
-print(api.wall.post(message="Жаль, что не все поймут) Не многие вспомнят))"))
+# print(api.wall.post(message="Жаль, что не все поймут) Не многие вспомнят))"))
+post_id = 21
+user_id = 521780797
+print(api.wall.createComment(owner_id=user_id, post_id=post_id, message="Да уж, не так уж много олдов в наше время)"))
